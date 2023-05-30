b7f05a245c38cadb067d5a9c2d8097676ec121fb
Azizbek <badirdinovazizbek5@gmail.com>
2023-05-30 15:49:27 +0600

    Aziz-2

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <div className="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom container pt-3">
+            <Link to={'/'}>
+                Logo
+            </Link>
+            <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+                <Link to={'/login'} className="me-3 py-2 link-body-emphasis text-decoration-none">Login</Link>
+                <Link to={'/register'} className="me-3 py-2 link-body-emphasis text-decoration-none">Register</Link>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
